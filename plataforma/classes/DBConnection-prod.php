b805a1a45ab4ad8e2c87d4429a1ca32a76ec8d78
Nicolas Galano <nicolas.galano@gmail.com>
2018-06-26 19:12:40 -0300

    latest

---
+++ plataforma/classes/DBConnection-prod.php
@@ -0,0 +1,45 @@
+<?php
+
+class DBConnection {
+
+    private static $db = null;
+    private function __construct() {}
+    /**
+     * Abre la conexión a la base de datos con PDO.
+     */
+    private static function openConnection()
+    {
+        $host = "localhost";
+        $user = "nomasbul_demo";
+        $pass = "DaVinci2018!";
+        $base = "nomasbul_demo";
+        $dsn = "mysql:host=$host;dbname=$base;charset=utf8";
+
+        self::$db = new PDO($dsn, $user, $pass);
+    }
+
+    /**
+     * Retorna una conexión a la base de datos en modo Singleton.
+     *
+     * @return PDO
+     */
+    public static function getConnection()
+    {
+        if(is_null(self::$db)) {
+            self::openConnection();
+        }
+        return self::$db;
+    }
+
+    /**
+     * Retorna el PDOStatement para el $query proporcionado.
+     *
+     * @param string $query
+     * @return PDOStatement
+     */
+    public static function getStatement($query)
+    {
+        return self::getConnection()->prepare($query);
+    }
+
+}
